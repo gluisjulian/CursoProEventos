fd90c8eba3be3704cd2c1da8510d0d6357a76ef7
gluisjulian <gluisjulian@gmail.com>
2022-12-12 18:37:45 -0300

    Adicionado Model Evento.

---
+++ BackEnd/src/ProEventos.API/ProEventos.API/Model/Evento.cs
@@ -0,0 +1,21 @@
+﻿namespace ProEventos.API.Model
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public DateTime? DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string ImagemURL { get; set; }
+        public string Lote { get; set; }
+
+
+
+
+
+        //public IEnumerable<Lote> Lotes { get; set; }
+        //public IEnumerable<RedeSocial> RedesSociais { get; set; }
+        //public IEnumerable<PalestranteEvento> PalestrantesEventos { get; set; }
+    }
+}
