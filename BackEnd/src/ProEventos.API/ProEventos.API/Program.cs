ff948cd055a5f1d5f4ca596c366b16e5bf6597a0
gluisjulian <gluisjulian@gmail.com>
2022-12-20 19:25:44 -0300

    Injeção de dependencia.

--- BackEnd/src/ProEventos.API/ProEventos.API/Program.cs
+++ BackEnd/src/ProEventos.API/ProEventos.API/Program.cs
@@ -1,12 +1,21 @@
 using Microsoft.EntityFrameworkCore;
+using ProEventos.Application.Interface;
+using ProEventos.Application.Interface.Implementations;
 using ProEventos.Persistence.Context;
+using ProEventos.Persistence.Interface;
+using ProEventos.Persistence.Interface.Implementations;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+//Database
 builder.Services.AddDbContext<ProEventosContext>(context => context.UseSqlServer(builder.Configuration.GetConnectionString("ProEventos")));
 
+
+//Dependecy Injection
+builder.Services.AddScoped<IEventoService, EventoService>();
+builder.Services.AddScoped<IEventoPersistence, EventoPersistence>();
+builder.Services.AddScoped<IGeralPersistence, GeralPersistence>();
 
 
 
